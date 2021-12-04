a3326a1cfdc372e77440b72b13247f3e436607ed
Vincent Hanquez <vincent@typed.io>
2021-12-04 21:52:24 +0800

    introduce new generic benchmark methods and add some curve25519 bench

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ fn human_unit(v: u128) -> String {
 }
 
 pub struct BenchData {
-    datalen: usize,
+    datalen: Option<usize>,
     tries: usize,
     counters: Vec<u64>,
     durations: Vec<Duration>,
@@ -61,8 +61,14 @@ impl BenchData {
             .checked_div(self.tries as u64)
             .expect("cannot div");
 
-        let speed = self.datalen as u128 * 1_000_000_000 / average_dur.as_nanos();
-        let speed_h = human_unit(speed);
+        let speed_string = match self.datalen {
+            None => String::new(),
+            Some(datalen) => {
+                let speed = datalen as u128 * 1_000_000_000 / average_dur.as_nanos();
+                let speed_h = human_unit(speed);
+                format!("=> {}{}", speed_h, Red.paint("/s"))
+            }
+        };
 
         let min = self.durations.iter().min().expect("min");
         let max = self.durations.iter().max().expect("max");
@@ -77,6 +83,7 @@ impl BenchData {
         fn print_dur(dur: Duration) -> String {
             // "1.234xy"
             let secs = dur.as_secs();
+            //let nanosecs = dur.subsec_nanos();
             if secs > 1 {
                 format!(
                     "{:3}.{:02}s ",
@@ -90,12 +97,12 @@ impl BenchData {
                 let nanos = (dur.subsec_nanos() % 1000) / 10;
                 format!("{:3}.{:02}us", dur.subsec_micros(), nanos)
             } else {
-                format!("{}ns", dur.subsec_nanos())
+                format!("{:3}ns", dur.subsec_nanos())
             }
         }
 
         println!(
-            "{:10} -- {:16}  [ {} .. ~{} .. {} ]   {}={} {} ; {}={} => {}{}",
+            "{:10} -- {:16}  [ {} .. ~{} .. {} ]   {}={} {} ; {}={} {}",
             name,
             package,
             Yellow.paint(&print_dur(*min)),
@@ -106,10 +113,42 @@ impl BenchData {
             Red.paint("ms"),
             Blue.paint("cycle"),
             Purple.paint(&format!("{}", average_cycles)),
-            speed_h,
-            Red.paint("/s"),
+            speed_string,
         );
     }
+}
+
+fn benchmark<F>(tries: usize, f: F) -> BenchData
+where
+    F: Fn() -> (),
+{
+    let mut durations = Vec::with_capacity(tries);
+    let mut counters = Vec::with_capacity(tries);
+
+    for _ in 0..tries {
+        let mut aux = 0u32;
+        let start = SystemTime::now();
+        let counter_start = counter(&mut aux);
+        f();
+        let counter_end = counter(&mut aux);
+        let end = SystemTime::now();
+        let dur = end
+            .duration_since(start)
+            .expect("Clock may have gone backwards");
+        durations.push(dur);
+        if counter_end >= counter_start {
+            counters.push(counter_end - counter_start);
+        } else {
+            counters.push(0)
+        }
+    }
+    let bd = BenchData {
+        datalen: None,
+        tries,
+        durations,
+        counters,
+    };
+    bd
 }
 
 fn benchmark_hash<C, F, G>(name: &str, package: &str, tries: usize, data: &[u8], new: F, update: G)
@@ -141,7 +180,7 @@ where
         }
     }
     let bd = BenchData {
-        datalen: data.len(),
+        datalen: Some(data.len()),
         tries,
         durations,
         counters,
@@ -151,6 +190,13 @@ where
 
     // cool down time
     std::thread::sleep(std::time::Duration::from_secs(1));
+}
+
+fn group<F>(_group_name: &str, f: F)
+where
+    F: FnOnce() -> (),
+{
+    f()
 }
 
 fn main() {
@@ -164,97 +210,138 @@ fn main() {
         };
     }
 
-    for _ in 0..3 {
-        // ********* BLAKE2B *******
-        {
-            use cryptoxide::{blake2b::Blake2b, digest::Digest};
-            bench_hash!("blake2b", "cryptoxide", Blake2b::new(64), |c, d| {
-                c.input(d)
-            });
-        }
+    let repeat = 3;
 
-        {
-            use blake2::{Blake2b, Digest};
-            bench_hash!("blake2b", "blake2", Blake2b::new(), |c, d| { c.update(d) });
-        }
+    // packages we bench
+    const PKG_CRYPTOXIDE: &str = "cryptoxide";
+    const PKG_DALEK: &str = "dalek";
+    const PKG_SHA2: &str = "sha2";
+    const PKG_BLAKE2: &str = "blake2";
+    const PKG_RING: &str = "ring";
 
-        // ********* BLAKE2S *******
+    for _ in 0..repeat {
+        group("blake2b", || {
+            {
+                use cryptoxide::{blake2b::Blake2b, digest::Digest};
+                bench_hash!("blake2b", PKG_CRYPTOXIDE, Blake2b::new(64), |c, d| {
+                    c.input(d)
+                });
+            }
 
-        {
-            use cryptoxide::{blake2s::Blake2s, digest::Digest};
-            bench_hash!("blake2s", "cryptoxide", Blake2s::new(32), |c, d| {
-                c.input(d)
-            });
-        }
+            {
+                use blake2::{Blake2b, Digest};
+                bench_hash!("blake2b", PKG_BLAKE2, Blake2b::new(), |c, d| {
+                    c.update(d)
+                });
+            }
+        });
 
-        {
-            use blake2::{Blake2s, Digest};
-            bench_hash!("blake2s", "blake2", Blake2s::new(), |c, d| { c.update(d) });
-        }
+        group("blake2s", || {
+            {
+                use cryptoxide::{blake2s::Blake2s, digest::Digest};
+                bench_hash!("blake2s", PKG_CRYPTOXIDE, Blake2s::new(32), |c, d| {
+                    c.input(d)
+                });
+            }
 
-        // ********* SHA256 *******
-        {
-            use cryptoxide::{digest::Digest, sha2::Sha256};
-            bench_hash!("sha256", "cryptoxide", Sha256::new(), |c, d| { c.input(d) });
-        }
+            {
+                use blake2::{Blake2s, Digest};
+                bench_hash!("blake2s", PKG_BLAKE2, Blake2s::new(), |c, d| {
+                    c.update(d)
+                });
+            }
+        });
 
-        {
-            use sha2::{Digest, Sha256};
-            bench_hash!("sha256", "sha2", Sha256::new(), |c, d| { c.update(d) });
-        }
+        group("sha256", || {
+            {
+                use cryptoxide::{digest::Digest, sha2::Sha256};
+                bench_hash!("sha256", PKG_CRYPTOXIDE, Sha256::new(), |c, d| {
+                    c.input(d)
+                });
+            }
 
-        {
-            use ring::digest;
-            bench_hash!(
-                "sha256",
-                "ring",
-                digest::Context::new(&digest::SHA256),
-                |c, d| { c.update(d) }
-            );
-        }
+            {
+                use sha2::{Digest, Sha256};
+                bench_hash!("sha256", PKG_SHA2, Sha256::new(), |c, d| { c.update(d) });
+            }
 
-        // ********* SHA512 *******
-        {
-            use cryptoxide::{digest::Digest, sha2::Sha512};
-            bench_hash!("sha512", "cryptoxide", Sha512::new(), |c, d| { c.input(d) });
-        }
+            {
+                use ring::digest;
+                bench_hash!(
+                    "sha256",
+                    PKG_RING,
+                    digest::Context::new(&digest::SHA256),
+                    |c, d| { c.update(d) }
+                );
+            }
+        });
 
-        {
-            use sha2::{Digest, Sha512};
-            bench_hash!("sha512", "sha2", Sha512::new(), |c, d| { c.update(d) });
-        }
+        group("sha512", || {
+            {
+                use cryptoxide::{digest::Digest, sha2::Sha512};
+                bench_hash!("sha512", PKG_CRYPTOXIDE, Sha512::new(), |c, d| {
+                    c.input(d)
+                });
+            }
 
-        {
-            use ring::digest;
-            bench_hash!(
-                "sha512",
-                "ring",
-                digest::Context::new(&digest::SHA512),
-                |c, d| { c.update(d) }
-            );
-        }
+            {
+                use sha2::{Digest, Sha512};
+                bench_hash!("sha512", PKG_SHA2, Sha512::new(), |c, d| { c.update(d) });
+            }
 
-        // ********* POLY1305 *******
+            {
+                use ring::digest;
+                bench_hash!(
+                    "sha512",
+                    PKG_RING,
+                    digest::Context::new(&digest::SHA512),
+                    |c, d| { c.update(d) }
+                );
+            }
+        });
 
-        {
-            use cryptoxide::{mac::Mac, poly1305::Poly1305};
-            let key = [2u8; 32];
-            bench_hash!("poly1305", "cryptoxide", Poly1305::new(&key), |c, d| {
-                c.input(d)
-            });
-        }
-        /*
-        {
-            use poly1305::Poly1305;
-            use universal_hash::NewUniversalHash;
-            let key = [2u8; 32];
-            bench_hash!(
-                "poly1305::poly1305",
-                Poly1305::new(key.as_ref().into()),
-                |c, d| { c.compute_unpadded(&d); }
-            );
-        }
-        */
+        group("poly1305", || {
+            {
+                use cryptoxide::{mac::Mac, poly1305::Poly1305};
+                let key = [2u8; 32];
+                bench_hash!("poly1305", PKG_CRYPTOXIDE, Poly1305::new(&key), |c, d| {
+                    c.input(d)
+                });
+            }
+            /*
+            {
+                use poly1305::Poly1305;
+                use universal_hash::NewUniversalHash;
+                let key = [2u8; 32];
+                bench_hash!(
+                    "poly1305::poly1305",
+                    Poly1305::new(key.as_ref().into()),
+                    |c, d| { c.compute_unpadded(&d); }
+                );
+            }
+            */
+        });
+
+        group("curve25519", || {
+            {
+                use cryptoxide::curve25519::curve25519_base;
+
+                let bd = benchmark(tries, || {
+                    let _out = curve25519_base(&[1; 32]);
+                    ()
+                });
+                bd.reports("curve25519", PKG_CRYPTOXIDE)
+            }
+            {
+                use x25519_dalek::{x25519, X25519_BASEPOINT_BYTES};
+
+                let bd = benchmark(tries, || {
+                    let _out = x25519([1; 32], X25519_BASEPOINT_BYTES);
+                    ()
+                });
+                bd.reports("curve25519", PKG_DALEK)
+            }
+        });
         println!("")
     }
 }
